54d0432775ba86fba02907c5a1be07245507fef6
Asma Hassan <asmasheikhhassan@gmail.com>
2023-03-28 23:33:01 +0100

    Added eventlistener to submit button and a function

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -18,3 +18,28 @@ function renderHighscores() {
 
 }
   
+
+submitButton.addEventListener("click", function(event) {
+    event.preventDefault();
+  
+    var initialInput = document.querySelector("#initials").value;
+  
+    if (initialInput === "") {
+      displayMessage("error", "Initials cannot be blank");
+    } else {
+      displayMessage("success", "Registered successfully");
+      
+      var currentScore = JSON.parse(localStorage.getItem("initials"));
+       
+      if (!currentScore){
+        currentScore = [];
+      }
+
+      currentScore.push(initialInput + ' ' + score);
+     
+      localStorage.setItem("initials", JSON.stringify(currentScore));
+
+      renderHighscores();
+    }
+});
+  
