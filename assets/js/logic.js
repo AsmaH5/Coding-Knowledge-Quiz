e8c11c073ec934bec291114609e5e254858d28bc
Asma Hassan <asmasheikhhassan@gmail.com>
2023-03-28 23:31:55 +0100

    Added function to get the highscores

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -6,4 +6,15 @@ var highScore = document.querySelector("#final-score");
 function displayMessage(type, message) {
     msgDiv.textContent = message;
     msgDiv.setAttribute("class", type);
-  }
+}
+
+
+function renderHighscores() {
+    var initial = localStorage.getItem("initials");
+
+    if (!initial) {
+    return;
+    }
+
+}
+  
