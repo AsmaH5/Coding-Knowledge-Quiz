72d0b8363a3a9f2e0f2d9abe0ba074bd63805271
Asma Hassan <asmasheikhhassan@gmail.com>
2023-03-28 23:35:02 +0100

    Added an if statement to append each highscores submitted after each other

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,3 +1,11 @@
 var initial = JSON.parse(localStorage.getItem("initials"));
 var list = document.getElementById("highscores");
 var clearHighscoresButton = document.querySelector("#clear");
+
+if (initial){
+    for(var i = 0; i < initial.length; i++){
+        var li = document.createElement("li");
+        li.innerText = initial[i];
+        list.appendChild(li);
+    }
+}
