6baaad38c859034ae01e7255c8f5a8e42eb075bb
Asma Hassan <asmasheikhhassan@gmail.com>
2023-03-28 23:30:49 +0100

    Added a function to display a message

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -2,3 +2,8 @@ var initialInput = document.querySelector("#initials");
 var submitButton = document.querySelector("#submit");
 var msgDiv = document.querySelector("#msg");
 var highScore = document.querySelector("#final-score");
+
+function displayMessage(type, message) {
+    msgDiv.textContent = message;
+    msgDiv.setAttribute("class", type);
+  }
